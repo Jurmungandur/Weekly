cdeb64084fd8c55d2ae98c980cc9f159eab56ded
Jurmungandur <31280587+Jurmungandur@users.noreply.github.com>
2017-11-09 09:28:04 +0100

    Game v1.0.1
    Made movment variables for the player.

--- Weekly/Assets/PlayerController.cs
+++ Weekly/Assets/PlayerController.cs
@@ -4,12 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private float xScale = 1;
+    private float yScale = 1;
+
+    private float hsp = 0;
+    private float vsp = 0;
+
+    private float masterSpeed = 1;
+
+    private float grndAcc;
+    private float grndMax;
+
+    private float airAcc;
+    private float airFric = 0.01f;
+    private float airSpeed;
+
+    private float jumpSpeed;
+    private bool jumpHold = false;
+
+    private bool grounded = false; 
+
+    void Start () {
+        grndAcc = 0.15f * masterSpeed;
+        grndMax = 4f * masterSpeed;
+
+        airAcc = 0.1f * masterSpeed;
+        airSpeed = 3.5f * masterSpeed;
+
+        jumpSpeed = 3 * masterSpeed;
+    }
 	
-	// Update is called once per frame
 	void Update () {
 		 
 	}
